13e1faa4ba306dcce940212c8e2f529950a18082
gf <1751242646@qq.com>
2019-06-09 00:41:14 +0800

    first commit

--- src/main/java/left/gefei/community/controller/IndexController.java
+++ src/main/java/left/gefei/community/controller/IndexController.java
@@ -1,13 +1,37 @@
 package left.gefei.community.controller;
 
+import left.gefei.community.mapper.UserMapper;
+import left.gefei.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    private String index(){
-       return "index";
+    private String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if(cookies !=null && cookies.length!=0){
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if(user != null){
+                        request.getSession().setAttribute("user",user);
+                    }
+                    break;
+                }
+            }
+        }
+
+
+        return "index";
     }
 }
