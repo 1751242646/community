9cf3dd375ff98ae43b656deae2255a233d26f4b0
gf <1751242646@qq.com>
2019-06-12 22:29:00 +0800

    first commit

--- src/main/java/left/gefei/community/controller/IndexController.java
+++ src/main/java/left/gefei/community/controller/IndexController.java
@@ -1,5 +1,6 @@
 package left.gefei.community.controller;
 
+import left.gefei.community.dto.PaginationDTO;
 import left.gefei.community.dto.QuestionDTO;
 import left.gefei.community.mapper.UserMapper;
 import left.gefei.community.model.User;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -23,7 +25,10 @@ public class IndexController {
 
     @GetMapping("/")
     private String index(HttpServletRequest request,
-                         Model model){
+                         Model model,
+                         @RequestParam(name = "page",defaultValue = "1") Integer page,
+                         @RequestParam(name = "size",defaultValue = "10") Integer size
+    ){
         Cookie[] cookies = request.getCookies();
         if(cookies !=null && cookies.length!=0){
             for (Cookie cookie : cookies) {
@@ -37,8 +42,8 @@ public class IndexController {
                 }
             }
         }
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
--- src/main/java/left/gefei/community/service/QuestionService.java
+++ src/main/java/left/gefei/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package left.gefei.community.service;
 
+import left.gefei.community.dto.PaginationDTO;
 import left.gefei.community.dto.QuestionDTO;
 import left.gefei.community.mapper.QuestionMapper;
 import left.gefei.community.mapper.UserMapper;
@@ -18,9 +19,23 @@ public class QuestionService {
     private QuestionMapper questionMapper;
     @Autowired
     private UserMapper userMapper;
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+
+        if(page<1){
+            page = 1;
+        }
+        if(page>paginationDTO.getTotalPage()){
+            page = paginationDTO.getTotalPage();
+        }
+
+
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
         for (Question question : questions) {
          User user = userMapper.finById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -28,6 +43,9 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+
+        return paginationDTO;
     }
 }
