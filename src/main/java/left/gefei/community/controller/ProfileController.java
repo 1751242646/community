a33606ec234bd3027c894bd16bfc6477a9955f37
gf <1751242646@qq.com>
2019-06-13 22:41:01 +0800

    first commit

--- src/main/java/left/gefei/community/controller/ProfileController.java
+++ src/main/java/left/gefei/community/controller/ProfileController.java
@@ -1,12 +1,58 @@
 package left.gefei.community.controller;
 
+import left.gefei.community.dto.PaginationDTO;
+import left.gefei.community.mapper.UserMapper;
+import left.gefei.community.model.User;
+import left.gefei.community.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProfileController {
-    @GetMapping("/profile")
-    public String profile(){
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/profile/{action}")
+    public String profile(HttpServletRequest request,
+                          @PathVariable(name = "action") String action,
+                          Model model,
+                          @RequestParam(name = "page",defaultValue = "1") Integer page,
+                          @RequestParam(name = "size",defaultValue = "5") Integer size){
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        if(cookies !=null && cookies.length!=0){
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if(user != null){
+                        request.getSession().setAttribute("user",user);
+                    }
+                    break;
+                }
+            }
+        }
+        if(user == null){
+            return "redirect:/";
+        }
+        if("questions".equals(action)){
+            model.addAttribute("section","questions");
+            model.addAttribute("sectionName","我的问题");
+        }else if("replies".equals(action)){
+            model.addAttribute("section","replies");
+            model.addAttribute("sectionName","最新回复");
+        }
+        PaginationDTO paginationDTO = questionService.list(user.getId(), page, size);
+        model.addAttribute("pagination",paginationDTO);
         return "profile";
     }
 }
--- src/main/java/left/gefei/community/mapper/QuestionMapper.java
+++ src/main/java/left/gefei/community/mapper/QuestionMapper.java
@@ -10,11 +10,20 @@ import java.util.List;
 
 @Mapper
 public interface QuestionMapper {
+
     @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
     void create(Question question);
 
     @Select("select * from question limit #{offset},#{size}")
     List<Question> list(@Param(value = "offset") Integer offset, @Param(value = "size") Integer size);
+
     @Select("SELECT COUNT(1) FROM question")
     Integer count();
+
+    /*where creator = #{userId} limit #{offset},#{size}*/
+    @Select("select * from question ")
+    List<Question> listByUserId(@Param(value = "userId") Integer userId, @Param(value = "offset") Integer offset, @Param(value = "size") Integer size);
+
+    @Select("select count(1) from question where creator = #{userId}")
+    Integer countByUserId(@Param(value = "userId") Integer userId);
 }
